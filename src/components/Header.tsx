df1c22211d77f00ef8fc62e71c2750199794e5c7
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-08-05 18:14:23 +0600

    logo style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = ({
         <div className=" max-w-7xl mx-auto h-16 flex items-center justify-between px-5 md:px-10 lg:px-16">
           <Link href={brandHref} className="text-xl font-bold text-gray-900">
             {" "}
-            <span className=" text-white px-2 p-0.5 mr-0.8 bg-sky-700 text-2xl rounded-bl-2xl rounded-tr-2xl">
+            <span className=" text-white px-2 p-1 mr-0.8 bg-gray-900 text-2xl rounded-bl-2xl rounded-tr-2xl">
              ecommerce-s
             </span>{" "}
           </Link>
