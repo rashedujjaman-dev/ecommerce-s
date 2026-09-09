9c09d1916b77244a09d6301932c879347c28050f
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-09-10 00:03:53 +0600

    product selections

--- src/components/ProductActions.tsx
+++ src/components/ProductActions.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+"use client"
 
-const ProductActions = ({product}) => {
+
+import { Product } from '@/types/product'
+import React, { useState } from 'react'
+
+interface ProductActionsProps{
+  product: Product;
+}
+
+const ProductActions = ({product}: ProductActionsProps) => {
+  const [selectedColor, setSelectedColor] =useState<string | undefined>(product.colors?.[0]);
+
+   const [selectedSize, setSelectedSize] =useState<string | undefined>(product.sizes?.[0]);
+
+   const [quantity, setQuantity] = useState<number>(1);
+
+   const handleSelectionChange = (selections:   {
+      color?: string;
+      size?: string;
+      quantity?: number;
+   }) => {
+      if( selections.color !== undefined) {
+        setSelectedColor(selections.color);
+      } if( selections.size !== undefined) {
+        setSelectedSize(selections.size);
+      } if( selections.quantity !== undefined) {
+        setQuantity(selections.quantity);
+      }
+   }
   return (
-    <div>ProductActions</div>
+    <>
+
+    </>
   )
 }
 
